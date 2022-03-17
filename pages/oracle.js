134972f1d6a36f45d04f49de1038fe5b5aa09e7e
Chris Staunton <stauntch@tcd.ie>
2022-03-17 21:22:11 +0000

    view ilk spot and value

--- pages/oracle.js
+++ pages/oracle.js
@@ -9,11 +9,12 @@ import jugContract from '../blockchain/jug'
 import nftContract from '../blockchain/nft'
 import daiContract from '../blockchain/dai'
 import oracleContract from '../blockchain/oracle'
+
 // import { NextFetchEvent } from 'next/server'
 
 
 
-const Oracle = () => {
+const RwaOracle = () => {
     const [error, setError] = useState('')
     const [vatAdress, setVatAddress] = useState('')
     const [user, setUser] = useState(null)
@@ -31,6 +32,9 @@ const Oracle = () => {
     const [repayment, setRepayment] = useState(null)
     const [daiContract1, setDaiContract1] = useState(null)
     const [oracle, setOracle] = useState(null)
+    const [newValue, setNewValue] = useState(null)
+    const [value, setValue] = useState(null)
+    const [adjustedValue, setAdjustedVal] = useState(null)
     // let web3
 
     const wad = Math.pow(10, 18) //18 decimals
@@ -40,12 +44,15 @@ const Oracle = () => {
 
     useEffect(() => {
 
+        if (oracle) getIlkValues()
+
 
         if (urn) getVatAddressHandler()
         if (urn && user) getCanHandler()
         if (urn && user) getVaultBalanceHandler()
+        // if (oracle) getIlkValues()
 
-    }, [urn, user])
+    }, [urn, user, oracle])
 
     // if (urn) {
     //     urn.events.Lock({
@@ -73,27 +80,70 @@ const Oracle = () => {
     //             setBal(event.returnValues[1])
     //         })
     //         .on('error', console.error);
+    // }
 
-    const getValue = () =>{
-        
+    const getIlkValues = async () => {
+
+        // console.log(oracle)
+
+
+
+
+        const ilkVals = await vat.methods.ilks(ilk).call()
+        const orcIlk = await oracle.methods.ilks(ilk).call()
+
+        const spot = ilkVals[3] / rad
+
+        // console.log(mat)
+
+        setValue(spot * orcIlk[6] / 100)
+        setAdjustedVal(spot)
+
+
+
     }
 
-    const getGood = () =>{
+    const changeValue = async () => {
+        console.log("changing to this: ", newValue)
+        const tmp = oracle.methods.ilks(ilk).call()[5]
+        setAdjustedVal(tmp)
 
-    }
+        try {
 
-    const changeValue = () =>{
-        try{
+            oracle.methods.dip(ilk, newValue.toString()).send({
+                from: user
+            })
 
-            //do this
-            // change oracle so only one change of value
-            // combine bump and dip
-
-        } catch(err) {
+        } catch (err) {
             setError(err.message)
         }
     }
-    
+
+
+    const getVatAddressHandler = async () => {
+        try {
+            const vatAdress = await urn.methods.vat().call()
+            setVatAddress(vatAdress)
+        } catch (err) {
+            setError(err.message)
+        }
+    }
+
+
+
+    const getVaultBalanceHandler = async () => {
+        // const accounts = await web3.eth.getAccounts();
+        const result = await vat.methods.urns(ilk, urnAddr).call()
+        const dai = await vat.methods.dai(urnAddr).call()
+        const rate = await vat.methods.ilks(ilk).call()
+        setDai(dai / ray)
+        setDebt((rate[1] * result[1]) / rad)
+        setBal(result[0] / wad)
+    }
+
+    // "0x0000000000000000000000000000000000000000000000000000000000000001"
+    // "0x5257413939392d41000000000000000000000000000000000000000000000000"
+
     //window.ethereum
     const connectWalletHandler = async () => {
         // alert("Connecting...")
@@ -109,8 +159,12 @@ const Oracle = () => {
                 const daiContract1 = daiContract(web3)
                 setDaiContract1(daiContract1)
 
-                const oracle = oracleContract(web3, "0x166D6C931dbF8783D25D7a609916965F283c03d6")
+                const oracle = oracleContract(web3)
                 setOracle(oracle)
+
+                console.log(oracle)
+
+                // getIlkValues()
 
 
                 // getCanHandler()
@@ -141,7 +195,7 @@ const Oracle = () => {
             <nav className='navbar mt-4 mb-4'>
                 <div className='container'>
                     <div className='navbar-brand'>
-                        <h1>RWA Vault Oracle</h1>
+                        <h1>RWA Vault</h1>
 
                     </div>
                     <div className='navbar-item'>
@@ -153,11 +207,41 @@ const Oracle = () => {
                     </div>
                 </div>
             </nav>
-            
-           
+            <section>
+                <div className='container'>
+                    <p>Oracle Address: 0x166D6C931dbF8783D25D7a609916965F283c03d6</p>
+                </div>
+            </section>
+
+
+            <section>
+                <div className='container has-text-danger'>
+                    <p>{error}</p>
+                </div>
+            </section>
+            <section>
+                <div className='container'>
+                    <p>Current Value: {value}</p>
+                    <p>Adjusted Value: {adjustedValue}</p>
+                </div>
+            </section>
+            <section>
+                <div className='container'>
+                    <input type='text' onChange={(e) => setNewValue(e.target.value * wad)}></input>
+                    <button className='button is-secondary' onClick={changeValue}>Change Value</button>
+                </div>
+            </section>
+            <section>
+                <div className='container'>
+                    <button className='button is-secondary' onClick={getIlkValues}>get vals</button>
+                </div>
+            </section>
+
+
+
         </div>
     )
 }
 
 
-export default Oracle;
+export default RwaOracle;
